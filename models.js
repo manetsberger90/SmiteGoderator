4ca4b604b55cd1e79a2a321b2e7504af86a3cbc4
Michael <manetsberger90@gmail.com>
2015-03-04 14:07:20 -0800

    Goderator - Work in Progress

---
+++ models.js
@@ -0,0 +1,325 @@
+// Data models for webapp
+var models = [
+    {
+        id: 0,
+        name: 'Agni',
+        role: 'Mage',
+    },
+    {
+        id: 1,
+        name: 'Ah Muzen Cab',
+        role: 'Hunter',
+    },
+    {
+        id: 2,
+        name: 'Anhur',
+        role: 'Hunter',
+    },
+    {
+        id: 3,
+        name: 'Ao Kuang',
+        role: 'Mage',
+    },
+    {
+        id: 4,
+        name: 'Anubis',
+        role: 'Mage',
+    },
+    {
+        id: 5,
+        name: 'Aphrodite',
+        role: 'Mage',
+    },
+    {
+        id: 6,
+        name: 'Apollo',
+        role: 'Hunter',
+    },
+    {
+        id: 7,
+        name: 'Arachne',
+        role: 'Assassin',
+    },
+    {
+        id: 8,
+        name: 'Ares',
+        role: 'Guardian',
+        
+    },
+    {
+        id: 9,
+        name: 'Artemis',
+        role: 'Hunter',
+        
+    },
+    {
+        id: 10,
+        name: 'Athena',
+        role: 'Guardian',
+    },
+    {
+        id: 11,
+        name: 'Awilix',
+        role: 'Assassin',
+    },
+    {
+        id: 12,
+        name: 'Bacchus',
+        role: 'Guardian',
+    },
+    {
+        id: 13,
+        name: 'Bakasura',
+        role: 'Assassin',
+    },
+    {
+        id: 14,
+        name: 'Bastet',
+        role: 'Assassin',
+    },
+    {
+        id: 15,
+        name: 'Bellona',
+        role: 'Warrior',
+    },
+    {
+        id: 16,
+        name: 'Cabrakan',
+        role: 'Guardian',
+    },
+    {
+        id: 17,
+        name: 'Chaac',
+        role: 'Warrior',
+    },
+    {
+        id: 18,
+        name: "Chang'e",
+        role: 'Mage',
+    },
+    {
+        id: 19,
+        name: 'Chronos',
+        role: 'Mage',
+    },
+    {
+        id: 20,
+        name: 'Cupid',
+        role: 'Hunter',
+    },
+    {
+        id: 21,
+        name: 'Fenrir',
+        role: 'Assassin',
+    },
+    {
+        id: 22,
+        name: 'Freya',
+        role: 'Mage',
+    },
+    {
+        id: 23,
+        name: 'Geb',
+        role: 'Guardian',
+    },
+    {
+        id: 24,
+        name: 'Guan Yu',
+        role: 'Warrior',
+    },
+    {
+        id: 25,
+        name: 'Hades',
+        role: 'Guardian',
+    },
+    {
+        id: 26,
+        name: 'He Bo',
+        role: 'Mage'
+    },
+    {
+        id: 27,
+        name: 'Hel',
+        role: 'Mage',
+    },
+    {
+        id: 28,
+        name: 'Hercules',
+        role: 'Warrior',
+    },
+    {
+        id: 29,
+        name: 'Hou Yi',
+        role: 'Hunter',
+    },
+    {
+        id: 30,
+        name: 'Hun Batz',
+        role: 'Assassin',
+    },
+    {
+        id: 31,
+        name: 'Isis',
+        role: 'Mage',
+    },
+    {
+        id: 32,
+        name: 'Janus',
+        role: 'Mage',
+    },
+    {
+        id: 33,
+        name: 'Kali',
+        role: 'Assassin',
+    },
+    {
+        id: 34,
+        name: 'Kukulkan',
+        role: 'Mage',
+        
+    },
+    {
+        id: 35,
+        name: 'Kumbakharna',
+        role: 'Guardian',
+    },
+    {
+        id: 36,
+        name: 'Loki',
+        role: 'Assassin',
+    },
+    {
+        id: 37,
+        name: 'Mercury',
+        role: 'Assassin',
+    },
+    {
+        id: 38,
+        name: 'Ne Zha',
+        role: 'Assassin',
+    },
+    {
+        id: 39,
+        name: 'Neith',
+        role: 'Hunter',
+    },
+    {
+        id: 40,
+        name: 'Nemesis',
+        role: 'Assassin',
+    },
+    {
+        id: 41,
+        name: 'Nox',
+        role: 'Mage',
+    },
+    {
+        id: 42,
+        name: 'Nu Wa',
+        role: 'Mage',
+    },
+    {
+        id: 43,
+        name: 'Odin',
+        role: 'Warrior',
+    },
+    {
+        id: 44,
+        name: 'Osiris',
+        role: 'Warrior',
+    },
+    {
+        id: 45,
+        name: 'Poseidon',
+        role: 'Mage',
+    },
+    {
+        id: 46,
+        name: 'Ra',
+        role: 'Mage',
+    },
+    {
+        id: 47,
+        name: 'Rama',
+        role: 'Hunter',
+    },
+    {
+        id: 48,
+        name: 'Scylla',
+        role: 'Mage',
+        
+    },
+    {
+        id: 49,
+        name: 'Serqet',
+        role: 'Assassin',
+    },
+    {
+        id: 50,
+        name: 'Sobek',
+        role: 'Guardian',
+    },
+    {
+        id: 51,
+        name: 'Sun Wukong',
+        role: ' Warrior',
+    },
+    {
+        id: 52,
+        name: 'Sylvanus',
+        role: 'Guardian',
+    },
+    {
+        id: 53,
+        name: 'Thanatos',
+        role: 'Assassin',
+    },
+    {
+        id: 54,
+        name: 'Thor',
+        role: 'Assassin',
+    },
+    {
+        id: 55,
+        name: 'Tyr',
+        role: 'Warrior',
+    },
+    {
+        id: 56,
+        name: 'Ullr',
+        role: 'Hunter'
+    },
+    {
+        id: 57,
+        name: 'Vamana',
+        role: 'Warrior',
+        
+    },
+    {
+        id: 58,
+        name: 'Vulcan',
+        role: 'Mage',
+    },
+    {
+        id: 59,
+        name: 'Xbalanque',
+        role: 'Hunter',
+    },
+    {
+        id: 60,
+        name: 'Ymir',
+        role: 'Guardian',
+    },
+    {
+        id: 61,
+        name: 'Zhong Kui',
+        role: 'Mage',
+        
+    },
+    {
+        id: 62,
+        name: 'Zeus',
+        role: 'Mage',
+    },
+]
+    
