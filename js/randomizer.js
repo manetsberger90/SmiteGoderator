0a075b4a926ebb5133ac7019f955ea65c7fe7501
Michael <manetsberger90@gmail.com>
2015-03-05 22:20:41 -0800

    lost

--- js/randomizer.js
+++ js/randomizer.js
@@ -5,7 +5,7 @@ var godRole = getGod(roll).role;
 var godBuildLink = getGod(roll).buildLink;
 var godGuide = getGod(roll).guideLink;
 
-    var $overlay = $('<div id="overlay">' + '<h1 class="godOverlay">' + '</hi>' + '<p id="name">' + godName + "</p>" + '<p id="role">' + godRole + '</p>' + '<p id="buildLink">' + '<a href="' + godBuildLink + '"> Recommended Build' + '</a></p>' + '</h1></div>');
+    var $overlayGod = $('<div id="overlay">' + '<h1 class="godOverlay id=godOverlay">' + '</hi>' + '<p id="name">' + godName + "</p>" + '<p id="role">' + godRole + '</p>' + '<p id="buildLink">' + '<a href="' + godBuildLink + '"> Recommended Build' + '</a></p>' + '</h1></div>');
 
 //JQUERY START
 
@@ -26,33 +26,16 @@ var godGuide = getGod(roll).guideLink;
 
 $(".roll-button").click(function(event){
     event.preventDefault();
-    console.log(getGod());
     $(this).hide();
-    $overlay.show();    
-    });
-
-$("body").append($overlay);
-
-
-////Problem 2: Close Button Overlay and replace with overlay containing information about randomly selected god.
-
-////Solution: Need to create overlay with God information/Builds/Guides styled with CSS including CSS.
+if (event) {
+ $overlayGod.show();   
+}
+});
 
 
 
-////Add Overlay.
+$("body").append($overlayGod);
 
-  //Attach overlay to randomly generated god selection.
-
-//Add ReRoll button to Overlay that initiates function again.
-
-
-
-  // Update Overlay with the image linked in the link.
-  // Show the Overlay.
-
-
-  // Get child's alt attribute and set caption.
 
 
 
